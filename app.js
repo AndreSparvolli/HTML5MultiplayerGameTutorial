211d2fb44f1c10774cadbb3c6b90cdabe3b6e273
McClane <andresparvolli@hotmail.com>
2016-04-25 23:10:41 -0300

    First Commit

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+var serv = require('http').Server(app);
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/client/index.html');
+});
+
+app.use('/client', express.static(__dirname + '/client'));
+
+serv.listen(2000);
+
+console.log('Server Started.');
+
+
+var io = require('socket.io')(serv, {});
+
+io.sockets.on('connection', function(socket) {
+    console.log('Socket Connection');
+
+    socket.on('happy', function(data) {
+        console.log('happy' + data.reason);
+    });
+
+    socket.emit('serverMsg', {
+        msg: 'hello',
+    });
+
+});
